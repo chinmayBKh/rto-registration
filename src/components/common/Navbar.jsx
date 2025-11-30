0e3790faecb2bb57715dd64ffd8b477dd9ff7f5b
chinmayBKh <chinmay.bonde.cmaug25@gmail.com>
2025-11-30 11:41:23 +0530

    .

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,7 +1,7 @@
 // src/components/Navbar.js
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 const Navbar = () => {
   const { user, logout } = useAuth();
